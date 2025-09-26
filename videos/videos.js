51c993e904318bffe7dee238ca652f2db9eda6e9
us-chill <lazarus378mh@gmail.com>
2025-09-27 02:40:15 +0700

    Create videos.js

---
+++ videos/videos.js
@@ -0,0 +1,14 @@
+const videos = [
+  {
+    title: "USA Travel Vlog",
+    url: "https://cdn.example.com/embed/abcd1234"
+  },
+  {
+    title: "Street Food USA",
+    url: "https://cdn.example.com/embed/efgh5678"
+  },
+  {
+    title: "4th of July Fireworks",
+    url: "https://cdn.example.com/embed/ijkl9012"
+  }
+];
